b3bf715a725cf2440e5567fad7546797fc92627a
L3oN <minhnhatt0908@gmail.com>
2024-12-21 12:28:25 +0700

    chore: configure Prettier with import sorting and Tailwind CSS plugin

---
+++ prettier.config.js
@@ -0,0 +1,32 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "crlf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "^types$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/styles/(.*)$",
+    "^@/app/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+}
